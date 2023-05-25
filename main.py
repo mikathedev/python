64ef813c7f8b02fbc188cee5de1797fa1c471cd5
HAHA-de <123972776+HAHA-de@users.noreply.github.com>
2023-05-25 15:40:18 +0000

    4gthtru67

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import mode
 import medienfinder
-
+import range
 i = input("1 mode, 2 medin")
 l= input('enter with a comma and space between')
 
 if i == '1':
     print(mode.mode(l))
-
+elif i=='3':
+    print(range.a(l))
 else :
     print(medienfinder.medein(l))
 
