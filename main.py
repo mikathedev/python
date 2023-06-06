45cf041e24491ea192617f954c978f6673fde6c0
ru23go <yowest11@hotmail.com>
2023-06-06 10:14:58 -0400

    made git repo

--- main.py
+++ main.py
@@ -2,16 +2,8 @@ import mode
 import medienfinder
 import range
 import mean
-i = input("1 mode, 2 medin, 3 range")
+
 l= input('enter with a comma and space between')
 
-if i == '1':
-    print(mode.mode(l))
-elif i=='3':
-    print(range.a(l))
-
-elif i == '4':
-    print(mean.mean(l))
-else :
-    print(medienfinder.medein(l))
+print("mean"+mean.mean(l)+" medien: "+medienfinder.medein(l)+" mode: "+mode.mode(l)+" mean: "+mean.mean(l))
 
