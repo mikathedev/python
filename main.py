46e25331f8814647b5dc7e7ebccd23389c295ea9
ru23go <yowest11@hotmail.com>
2023-06-07 12:28:06 -0400

    made git repo

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ import mean
 
 l= input('enter with a comma and space between')
 
-print("mean: "+str(mean.mean(l))+" medien: "+str(medienfinder.medein(l))+" mode: "+str(mode.mode(l))+" mean: "+str(mean.mean(l)))
+print("mean: "+str(mean.mean(l))+" medien: "+str(medienfinder.medein(l))+" mode: "+str(mode.mode(l)))
 
